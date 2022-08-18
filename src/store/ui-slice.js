fd6e26a2fce0d87603f1200be67c114e1c1db248
Sahibzada Waseem Ahmad <wasim_ahmad2003@yahoo.com>
2022-08-18 16:29:03 +0500

    First commit

---
+++ src/store/ui-slice.js
@@ -0,0 +1,22 @@
+import { createSlice, TaskAbortError } from "@reduxjs/toolkit";
+
+const uiSlice = createSlice({
+  name: "ui",
+  initialState: { cartIsVisible: false, notification: null },
+  reducers: {
+    toggle(state) {
+      state.cartIsVisible = !state.cartIsVisible;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.messasge,
+      };
+    },
+  },
+});
+
+export const uiActions = uiSlice.actions;
+
+export default uiSlice;
